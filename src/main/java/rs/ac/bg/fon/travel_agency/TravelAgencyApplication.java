4529700a9c2b0be1d6f3085722daaf478fe4acd9
Anja <pejovic.anja208@gmail.com>
2025-06-22 13:06:31 +0200

    Initialize project with DB connection

---
+++ src/main/java/rs/ac/bg/fon/travel_agency/TravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.travel_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyApplication.class, args);
+	}
+
+}
